85eda9701f7315ec36def3de61b0befbb71e934c
Mariugo Megiani <mariugomegiani@gmail.com>
2020-09-14 11:13:46 -0300

    Spring project init

---
+++ src/test/java/com/mariugo/registroatividades/RegistroDeAtividadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariugo.registroatividades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroDeAtividadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
