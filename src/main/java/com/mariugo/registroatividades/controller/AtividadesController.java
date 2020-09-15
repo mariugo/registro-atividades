cd0c625fb2b59f6341d476455d4e595203b7760b
Mariugo Megiani <mariugomegiani@gmail.com>
2020-09-15 12:36:27 -0300

    Remoção do async para implementação no cliente

--- src/main/java/com/mariugo/registroatividades/controller/AtividadesController.java
+++ src/main/java/com/mariugo/registroatividades/controller/AtividadesController.java
@@ -5,25 +5,23 @@ import com.mariugo.registroatividades.repository.AtividadesRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 @AllArgsConstructor
 public class AtividadesController {
 
     private final AtividadesRepository atividadesRepository;
 
-    @Async
     @GetMapping("/atividades")
     public Iterable<Atividades> atividades(){
         return atividadesRepository.findAll();
     }
 
-    @Async
     @GetMapping("/atividade/{idAtividade}")
     public ResponseEntity<Atividades> atividade(@PathVariable(required = true, name = "idAtividade") Long idAtividade){
         Optional<Atividades> atividade = atividadesRepository.findById(idAtividade);
@@ -33,14 +31,12 @@ public class AtividadesController {
                 orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @Async
     @PostMapping("/nova_atividade") //INSERT INTO atividade
     public Atividades novaAtividade(@RequestBody Atividades atividade){
         atividadesRepository.save(atividade);
         return atividade;
     }
 
-    @Async
     @DeleteMapping("/remover_atividade/{idAtividade}")
     public ResponseEntity<Atividades> removerAtividade(@PathVariable(required = true, name = "idAtividade") Long idAtividade){
         if (atividadesRepository.findById(idAtividade).isPresent()) {
@@ -50,8 +46,7 @@ public class AtividadesController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @Async
-    @PutMapping("/atualizar_atividade/{idAtividade}") // UPDATE BY ID
+    @PatchMapping("/atualizar_atividade/{idAtividade}") // UPDATE BY ID
     public @ResponseBody ResponseEntity<Atividades> atualizarAtividade(@PathVariable(required = true, name = "idAtividade") Long idAtividade, @RequestBody Atividades atividade){
         Optional<Atividades> a = atividadesRepository.findById(idAtividade);
         if (a.isPresent()){
@@ -64,7 +59,6 @@ public class AtividadesController {
         return ResponseEntity.status(404).build();
     }
 
-    @Async
     @GetMapping("/contar_atividades")
     public Long contarAtividades(){
         return atividadesRepository.contarAtividades();
--- src/main/java/com/mariugo/registroatividades/controller/CategoriasController.java
+++ src/main/java/com/mariugo/registroatividades/controller/CategoriasController.java
@@ -5,27 +5,24 @@ import com.mariugo.registroatividades.repository.CategoriasRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 @AllArgsConstructor
 public class CategoriasController {
 
     private final CategoriasRepository categoriasRepository;
 
-    @Async
     @GetMapping("/categorias") //SELECT * FROM CATEGORIAS
     public Iterable<Categorias> categorias() {
         return categoriasRepository.findAll();
     }
 
     //Melhor utilizar a ResponseEntity para ter o status 404 e não retornar null
-    @Async
     @GetMapping("/categoria/{idCategoria}") // SELECT NOME FROM CATEGORIAS WHERE IDCATEGORIA=?
     public ResponseEntity<Categorias> categoria(@PathVariable(required = true, name = "idCategoria") Long idCategoria) {
         Optional<Categorias> categoria = categoriasRepository.findById(idCategoria);
@@ -35,14 +32,12 @@ public class CategoriasController {
                 orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @Async
     @PostMapping("/nova_categoria") //INSERT INTO CATEGORIA
     public Categorias novaCategoria(@RequestBody Categorias categoria){
         categoriasRepository.save(categoria);
         return categoria;
     }
 
-    @Async
     @DeleteMapping("/remover_categoria/{idCategoria}")
     public ResponseEntity<Categorias> removerCategoria(@PathVariable(required = true, name = "idCategoria") Long idCategoria){
         if (categoriasRepository.findById(idCategoria).isPresent()) {
@@ -52,18 +47,15 @@ public class CategoriasController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @Async
-    @PutMapping("/atualizar_categoria/{idCategoria}")
+    @PatchMapping("/atualizar_categoria/{idCategoria}")
     public ResponseEntity<Categorias> atualizarCategoria(@PathVariable(required = true, name = "idCategoria") Long idCategoria, @RequestBody Categorias categoria){
         Categorias c = categoriasRepository.save(categoria);
         return ResponseEntity.ok().body(c);
     }
 
-    @Async
     @GetMapping("/contar_categorias")
     public Long contarCategorias(){
         return categoriasRepository.contarCategorias();
     }
-
 
 }
